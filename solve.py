5660a91e733004ac00b33ea3723e39b23cdf0cd7
parthparikh <parthparikh1999p@gmail.com>
2019-12-15 19:22:13 +0530

    Added method to create a z3 constraint, which for all clues, chooses one guess from all the guesses Added method to convert each guess of a clue in it's decimal equivalent form     Example: "mumbai" -> [109, 117, 109, 98, 97, 105]

--- solve.py
+++ solve.py
@@ -54,26 +54,90 @@ class Solve():
 		matrix = [[None for index in range(max_val)] for index in range(max_val)]
 
 		# The following code encodes the matrix declared with z3.Int(VALUE)
-		# If at a position value is: (z3.Int(VALUE), z3.Int(VALUE)), it signifies intersection is taking place there
+		# If at a position value is: (z3.Int(VALUE)_1, z3.Int(VALUE)_2), it signifies intersection is taking place there
+		# NOTE: _1 -> signifies the COLUMN index AND _2 -> signifies the ROW index
 		for x_index in range(max_val):
 			for y_index in range(max_val):
+
 				if (x_index, y_index) in list(start_positions.keys()):
 					pos_info = GRID[start_positions[(x_index, y_index)]]
 					for i in range(pos_info["length"]):
 
 						if pos_info["direction"] == "A":
 							if isinstance(matrix[x_index][y_index + i], z3.z3.ArithRef):
-								matrix[x_index][y_index + i] = (matrix[x_index][y_index + i], z3.Int("alpha_" + str(x_index) + "_" + str(y_index + i)))
+								matrix[x_index][y_index + i] = (z3.Int("alpha_" + str(x_index) + "_" + str(y_index + i) + "_1"), z3.Int("alpha_" + str(x_index) + "_" + str(y_index + i) + "_2"))
 							else:
 								matrix[x_index][y_index + i] = z3.Int("alpha_" + str(x_index) + "_" + str(y_index + i))
 
 						elif pos_info["direction"] == "D":
 							if isinstance(matrix[x_index + i][y_index], z3.z3.ArithRef):
-								matrix[x_index + i][y_index] = (matrix[x_index + i][y_index], z3.Int("alpha_" + str(x_index + i) + "_" + str(y_index)))
+								matrix[x_index + i][y_index] = (z3.Int("alpha_" + str(x_index + i) + "_" + str(y_index) + "_1"), z3.Int("alpha_" + str(x_index + i) + "_" + str(y_index) + "_2"))
 							else:
 								matrix[x_index + i][y_index] = z3.Int("alpha_" + str(x_index + i) + "_" + str(y_index))
 
-		return matrix
+		return matrix, start_positions, max_val
+
+	def convert_clues_code(self):
+		"""converts all the clues (in string format) to a list of their ascii characters value
+		   example: "mumbai" -> [109, 117, 109, 98, 97, 105]
+		"""
+		clues = self.fetch_clues()
+		clues_ord = dict()
+
+		for clue in clues:
+			for guess in clues[clue]:
+				try:
+					clues_ord[clue].append([ord(ch) for ch in guess.lower()])
+				except:
+					clues_ord[clue] = [[ord(ch) for ch in guess.lower()]]
+
+		return clues_ord
+
+	def make_guess_constraint(self):
+		clues = self.convert_clues_code()
+		matrix, start_positions, max_val = self.get_matrix()
+
+		solver = z3.Solver()
+		clue_constraint = list()
+
+		for x_index in range(max_val):
+			for y_index in range(max_val):
+				if (x_index, y_index) in list(start_positions.keys()):
+					pos_info = GRID[start_positions[(x_index, y_index)]]
+					clue = start_positions[(x_index, y_index)]
+
+					guesses_ord = clues[clue]
+					all_guesses_constraint = list()
+
+					for guess_ord in guesses_ord:
+						guess_constraint = list()
+						x_i = x_index
+						y_i = y_index
+
+						for ch in guess_ord:
+							if isinstance(matrix[x_i][y_i], tuple):
+								# NOTE: _1 -> signifies the COLUMN index AND _2 -> signifies the ROW index
+								if pos_info["direction"] == "D":
+									matrix_val = matrix[x_i][y_i][0]
+								elif pos_info["direction"] == "A":
+									matrix_val = matrix[x_i][y_i][1]
+							else:
+								matrix_val = matrix[x_i][y_i]
+
+							guess_constraint.append(z3.And(matrix_val == ch))
+
+							if pos_info["direction"] == "D":
+								x_i += 1
+							elif pos_info["direction"] == "A":
+								y_i += 1
+
+						all_guesses_constraint.append(z3.And(guess_constraint))
+
+					clue_constraint.append(z3.Or(all_guesses_constraint))
+
+		clues_constraint = z3.And(clue_constraint)
+
+		return clues_constraint
 
 if __name__ == '__main__':
 	pass
