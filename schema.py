d8b8b5be6dcd1132d8bae80b5f9a2ff74dcac4b1
parthparikh <parthparikh1999p@gmail.com>
2019-12-20 16:34:53 +0530

    Added schema.py This file contains a global variable containing the schema of the entire crossword For each clue, it contains clue name, start index, direction and length of the clue

---
+++ schema.py
@@ -0,0 +1,17 @@
+# Pattern is (initial_X, initial_Y), direction(D or A), length
+# THE WORDS INSERTED SHOULD HAVE THEIR STARTING LETTER CAPITALIZED (For clue names)
+# "": {"start":(), "direction":"", "length": },
+CROSSWORD_GRID = {
+	"__ of bad news": {"start":(0, 1), "direction":"D", "length": 6},
+	"Posture problem": {"start":(0, 3), "direction":"D", "length": 5},
+	"Loads": {"start":(0, 4), "direction":"D", "length": 6},
+	"Laundry appliance": {"start":(0, 5), "direction":"D", "length": 5},
+	"Lectured": {"start":(1, 0), "direction":"D", "length": 5},
+	"One who weeps": {"start":(1, 2), "direction":"D", "length": 5},
+	"Grassy clump": {"start":(0, 3), "direction":"A", "length": 3},
+	"Pie chart portion": {"start":(1, 0), "direction":"A", "length": 6},
+	"\"Scary Movie,\" e.g.": {"start":(2, 0), "direction":"A", "length": 6},
+	"Maryland's state bird": {"start":(3, 0), "direction":"A", "length": 6},
+	"Something worth saving": {"start":(4, 0), "direction":"A", "length": 6},
+	"\"To __ is human\"": {"start":(5, 0), "direction":"A", "length": 3}
+}
--- solve.py
+++ solve.py
@@ -3,6 +3,7 @@ from tabulate import tabulate
 from file_path import *
 import json
 import z3
+from schema import CROSSWORD_GRID
 
 """
 NOTE: No length-verification is required in Solve(),
@@ -11,20 +12,7 @@ NOTE: No length-verification is required in Solve(),
 
 # Pattern is (initial_X, initial_Y), direction(D or A), length
 # "": {"start":(), "direction":"", "length": },
-GRID = {
-	"__ of bad news": {"start":(0, 1), "direction":"D", "length": 6},
-	"Posture problem": {"start":(0, 3), "direction":"D", "length": 5},
-	"Loads": {"start":(0, 4), "direction":"D", "length": 6},
-	"Laundry appliance": {"start":(0, 5), "direction":"D", "length": 5},
-	"Lectured": {"start":(1, 0), "direction":"D", "length": 5},
-	"One who weeps": {"start":(1, 2), "direction":"D", "length": 5},
-	"Grassy clump": {"start":(0, 3), "direction":"A", "length": 3},
-	"Pie chart portion": {"start":(1, 0), "direction":"A", "length": 6},
-	"\"Scary Movie,\" e.g.": {"start":(2, 0), "direction":"A", "length": 6},
-	"Maryland's state bird": {"start":(3, 0), "direction":"A", "length": 6},
-	"Something worth saving": {"start":(4, 0), "direction":"A", "length": 6},
-	"\"To __ is human\"": {"start":(5, 0), "direction":"A", "length": 3}
-}
+GRID = CROSSWORD_GRID
 
 class Solve():
 	def __init__(self):
